13aad0a13ba39de0815b8751e158a5390c8323f7
aiglazunov <dr_west@mail.ru>
2021-01-08 22:49:06 +0300

    version 1.1

--- src/main/java/web/controller/AdminController.java
+++ src/main/java/web/controller/AdminController.java
@@ -43,57 +43,30 @@ public class AdminController {
         //Получим всех людей из DAO и передадим на отображение в представление
         return "admin/index";
     }
-    // создание нового пользователя
-    @GetMapping("/admin/new")
-    public String newUser(Model model) {
-        model.addAttribute("user", new User());
-        model.addAttribute("allRoles", roleService.allRoles());
-        return "admin/new";
-    }
-    @PostMapping("/admin/new2")
-    public String create2(HttpServletRequest request) {
-        User user = new User();
 
-        user.setUsername(request.getParameter("username").trim());
-        user.setPassword(request.getParameter("password").trim());
-        user.setName(request.getParameter("name").trim());
-        user.setLastName(request.getParameter("lastname").trim());
-        user.setAge(Byte.valueOf(request.getParameter("age").trim()));
-        String[] roles = request.getParameterValues("allRoles");
-        Set<Role> roleSet = new HashSet<>();
-        for (String role : roles) {
-            //System.out.println(role);
-            roleSet.add(roleService.findRoleByName(role));
-        }
-        user.setRoles(roleSet);
-        userService.add(user);
-
-        return "redirect:/admin";
-    }
     // сохранение нового пользователя
     @PostMapping("/admin/new")
     public String create(@ModelAttribute("user") User user,
-                         @RequestParam(value = "allRoles") String[] roles) {
+                         @RequestParam(value = "allRoles", required = false) String[] roles) {
 
         //System.out.println(user);
         Set<Role> roleSet = new HashSet<>();
-        for (String role : roles) {
-            //System.out.println(role);
-            roleSet.add(roleService.findRoleByName(role));
+        if ( roles != null) {
+            for (String role : roles) {
+                //System.out.println(role);
+                roleSet.add(roleService.findRoleByName(role));
+            }
+
+        }
+        else {
+            roleSet.add(roleService.findRoleByName("ROLE_USER"));
         }
         user.setRoles(roleSet);
         userService.add(user);
 
         return "redirect:/admin";
     }
-    // редактирования пользователя
-    @GetMapping("/admin/edit")
-    public String edit(Model model, @RequestParam(value = "userid", required = true) long userid) {
-        model.addAttribute("user", userService.getById(userid));
-        //model.addAttribute("allRoles",  new ArrayList<>(userService.show(id).getRoles()));
-        model.addAttribute("allRoles", roleService.allRoles());
-        return "admin/edit";
-    }
+
     // сохранение редактирования пользователя
     @PostMapping("/admin/edit")
     public String update(@ModelAttribute("user") User user,
@@ -111,15 +84,6 @@ public class AdminController {
         return "redirect:/admin";
     }
 
-
-    @GetMapping("admin/{id}/delete")
-    public String delete(@PathVariable("id") long id) {
-        User user = userService.getById(id);
-        userService.delete(user);
-        return "redirect:/admin";
-    }
-
-
     // удаление пользователя
     @PostMapping("/admin/delete")
     public String delete2(@ModelAttribute("user") User user) {
@@ -127,31 +91,6 @@ public class AdminController {
         //model.addAttribute("allRoles",  new ArrayList<>(userService.show(id).getRoles()));
         userService.delete(user);
         return "redirect:/admin";
-    }
-
-    //стартовая страница для Админа
-    @GetMapping("/admin/admin")
-    public String adminPage(Principal principal, ModelMap modelMap) {
-
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-
-        Set<String> roles = authentication
-                .getAuthorities()
-                .stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.toSet());
-
-        StringBuilder sb = new StringBuilder();
-        for (String role : roles) {
-            sb.append(role.replaceAll("ROLE_", "")).append(" ");
-        }
-
-
-        modelMap.addAttribute("username", principal.getName());
-        modelMap.addAttribute("roleSet", new String(sb));
-
-        return "admin/admin";
     }
 
     // страница с информацией о пользователе
